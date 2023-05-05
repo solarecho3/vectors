c0753aa110c8ace700e41d2692db2f1bab74ab88
solarecho3 <solarecho3@pm.me>
2023-05-05 12:20:40 -0400

    Colors and transitions for encoding segment

--- scene1.py
+++ scene1.py
@@ -1,23 +1,24 @@
 import manim
 import numpy
 
+
 class EncodeText(manim.Scene):
 
     #
     def construct(self):
-        quote = manim.Tex('\"', 'Fall ', 'to ', 'thy ', 'prayers', '...', '\"').scale(1)
+        _quote = manim.Tex('\"', 'Fall ', 'to ', 'thy ', 'prayers', '...', '\"').scale(1)
         quote_name = manim.Tex("- William Shakespeare").shift([3.0,-1.0,0]).scale(.5)
 
         # FADE IN: "Fall to the prayers..."
         # define the animations array as fadein
         animations = [
-            manim.FadeIn(quote[0]),
-            manim.FadeIn(quote[1]),
-            manim.FadeIn(quote[2]),
-            manim.FadeIn(quote[3]),
-            manim.FadeIn(quote[4]),
-            manim.FadeIn(quote[5]),
-            manim.FadeIn(quote[6]),
+            manim.FadeIn(_quote[0]),
+            manim.FadeIn(_quote[1]),
+            manim.FadeIn(_quote[2]),
+            manim.FadeIn(_quote[3]),
+            manim.FadeIn(_quote[4]),
+            manim.FadeIn(_quote[5]),
+            manim.FadeIn(_quote[6]),
             manim.FadeIn(quote_name)
         ]
         # Fade in the animations array
@@ -26,25 +27,25 @@ class EncodeText(manim.Scene):
 
         animations = [
             manim.FadeOut(quote_name),
-            manim.FadeOut(quote[0]),
-            manim.FadeOut(quote[6]),
+            manim.FadeOut(_quote[0]),
+            manim.FadeOut(_quote[6]),
         ]
         self.play(manim.AnimationGroup(*animations))
         self.wait(2)
 
         # FADE OUT: "Fall to the prayers..."
         animations = [
-            manim.FadeOut(quote[1]),
-            manim.FadeOut(quote[2]),
-            manim.FadeOut(quote[3]),
-            manim.FadeOut(quote[4]),
-            manim.FadeOut(quote[5]),
+            manim.FadeOut(_quote[1]),
+            manim.FadeOut(_quote[2]),
+            manim.FadeOut(_quote[3]),
+            manim.FadeOut(_quote[4]),
+            manim.FadeOut(_quote[5]),
         ]
-        self.play(manim.AnimationGroup(*animations))
+        # self.play(manim.AnimationGroup(*animations))
 
         # FADE IN: "Fall_to_thy_prayers...\n"
         # expose all characters
-        quote = manim.Tex('Fall', r' \_ ', 'to ', r'\_ ', 'thy ', r'\_ ', 'prayers ', '... ', r'\textbackslash n').scale(1)
+        quote = manim.Tex('Fall', r'\_', 'to', r'\_', 'thy', r'\_', 'prayers', '...', r'\textbackslash n').scale(1)
         # define the animations for transformation
         animations = [
             manim.FadeIn(quote[0]),
@@ -57,7 +58,9 @@ class EncodeText(manim.Scene):
             manim.FadeIn(quote[7]),
             manim.FadeIn(quote[8])
         ]
-        self.play(manim.AnimationGroup(*animations, lag_ratio=0.2))
+        # self.play(manim.AnimationGroup(*animations, lag_ratio=0.2))
+        # delete the quotes from the list
+        self.play(manim.FadeTransform(_quote[1:6], quote))
         self.wait(3)
 
         # FADE OUT: "Fall_to_thy_prayers...\n"
@@ -77,13 +80,15 @@ class EncodeText(manim.Scene):
 
         # SCALE, SHIFT: "Fall"
         self.play(manim.ScaleInPlace(quote[0], 2))
-        self.play(quote[0].animate.shift(manim.RIGHT*2.6))
+        self.play(quote[0].animate.shift(manim.RIGHT*2))
         self.wait(3)
 
         # FADE OUT: "Fall"
         self.play(manim.FadeOut(quote[0], shift=manim.DOWN))
 
         # FADE IN: Encoding table
+        # FADE COLOR: Encoded text
+        # FADE OUT: Encoding table
         t1 = manim.Table(
             [['F', 'a', 'l', 'l'],
              ['18', '39', '50', '50']],
@@ -99,8 +104,12 @@ class EncodeText(manim.Scene):
         t1.add(t1.get_cell((1, 5), color='#fbb003'))
         t1.add(t1.get_cell((2, 5), color='#fbb003'))
         self.play(manim.Write(t1))
-        self.wait(2)
-        self.play(manim.Unwrite(t1))
+        t1.get_entries(pos=(2, 2)).set_color('#3174f0')
+        t1.get_entries(pos=(2, 3)).set_color('#e53125')
+        t1.get_entries(pos=(2, 4)).set_color('#fbb003')
+        t1.get_entries(pos=(2, 5)).set_color('#fbb003')
+        self.wait(4)
+        # self.play(manim.Unwrite(t1))
 
         # FADE IN: "18 39 50 50"
         quote = manim.Tex('18',',','39',',','50',',','50').scale(3)
@@ -117,8 +126,10 @@ class EncodeText(manim.Scene):
         quote[2].color = '#e53125'
         quote[4].color = '#fbb003'
         quote[6].color = '#fbb003'
-        self.play(manim.AnimationGroup(*animations, lag_ratio=0))
-        self.wait(3)
+        # self.play(manim.AnimationGroup(*animations, lag_ratio=0))
+        # self.wait(3)
+
+        self.play(manim.FadeTransform(t1, quote))
         # FADE OUT: commas
         animations = [
             manim.FadeOut(quote[1]),
@@ -164,8 +175,9 @@ class EncodeText(manim.Scene):
                 speedinfo={1:4}
             )
         )
+        self.wait(3)
 
-        self.wait(1)
+        # FADE OUT: " F a l l "
         animations = [
             manim.FadeOut(_quote1),
             manim.FadeOut(_quote2),
@@ -175,13 +187,13 @@ class EncodeText(manim.Scene):
         self.play(manim.AnimationGroup(*animations))
         self.wait(1)
 
+        # draw brace and label
         brace1 = manim.BraceBetweenPoints([-5,-1,0], [5,-1,0], sharpness=0.7)
         brace1_txt = manim.Text('"encoded"').next_to(brace1, manim.DOWN*3)
         self.play(manim.GrowFromCenter(brace1))
         self.play(manim.FadeIn(brace1_txt))
-
+        # randomly select a different index of the string to change color
         for i in range(50):
-            # randomly select a different index of the string to change color
             colors = ['#3174f0','#e53125','#fbb003','#269a43']
             self.play(
                 manim.ChangeSpeed(
@@ -192,6 +204,7 @@ class EncodeText(manim.Scene):
                     speedinfo={.01:50}
                 )
             )
+        # set final color scheme to match encoded integers
         brace1_txt[1].set_color('#3174f0')
         brace1_txt[2].set_color('#e53125')
         brace1_txt[3].set_color('#e53125')
@@ -199,5 +212,115 @@ class EncodeText(manim.Scene):
         brace1_txt[5].set_color('#fbb003')
         brace1_txt[6].set_color('#fbb003')
         brace1_txt[7].set_color('#fbb003')
+        self.wait(5)
 
+        # FADE OUT: Brace and brace text
+        animations = [
+            manim.FadeOut(brace1),
+            manim.FadeOut(brace1_txt)
+        ]
+        self.play(manim.AnimationGroup(*animations))
         self.wait(3)
+
+        # FADE COLOR: Encoded text fade to white, wait 3
+        animations = [
+            manim.FadeToColor(quote[0], color=manim.WHITE),
+            manim.FadeToColor(quote[2], color=manim.WHITE),
+            manim.FadeToColor(quote[4], color=manim.WHITE),
+            manim.FadeToColor(quote[6], color=manim.WHITE),
+        ]
+        self.play(manim.AnimationGroup(*animations))
+        # self.wait(1)
+
+        # SPREAD IN: "18 39 50 50"
+        # FADE IN: Commas
+        animations = [
+            quote[0].animate(run_time=.7).shift(manim.RIGHT * 2),
+            quote[2].animate(run_time=.7).shift(manim.RIGHT * .5),
+            quote[6].animate(run_time=.7).shift(manim.LEFT * 2),
+            quote[4].animate(run_time=.7).shift(manim.LEFT * .5),
+            manim.FadeIn(quote[1]),
+            manim.FadeIn(quote[3]),
+            manim.FadeIn(quote[5]),
+        ]
+        self.play(manim.AnimationGroup(*animations))
+        self.wait(3)
+
+        # FADE IN: Brackets of the array
+        bracket1 = manim.Tex("[", "]").scale(3)
+        brace_2 = manim.BraceLabel(quote, "array", buff=.5)
+        animations = [
+            manim.FadeIn(bracket1[0], shift=manim.LEFT*5),
+            manim.FadeIn(bracket1[1], shift=manim.RIGHT*5),
+            bracket1[0].animate(run_time=.7).shift(manim.LEFT*3.7, manim.UP*.1),
+            bracket1[1].animate(run_time=.7).shift(manim.RIGHT*3.7, manim.UP*.1),
+            manim.FadeIn(brace_2)
+        ]
+        self.play(manim.AnimationGroup(*animations))
+        self.wait(3)
+
+        # FADE TO COLOR: Encoded text back to color
+        animations = [
+            manim.FadeToColor(quote[0], color='#3174f0'),
+            manim.FadeToColor(quote[2], color='#e53125'),
+            manim.FadeToColor(quote[4], color='#fbb003'),
+            manim.FadeToColor(quote[6], color='#fbb003')
+        ]
+        self.play(manim.AnimationGroup(*animations))
+
+        # FADE OUT: Brace and brackets
+        animations = [
+            manim.FadeOut(bracket1[0]),
+            manim.FadeOut(bracket1[1]),
+            manim.FadeOut(brace_2),
+        ]
+        self.play(manim.AnimationGroup(*animations))
+        self.wait(1)
+
+        # FADE AND SHRINK: Array into longer encoding
+        # FADE IN: Encoded paragraph
+        encoded_paragraph = manim.Text(
+            "[18, 39, 50, 50, 1, 58, 53, 1, 58, 46, 63, 1, 54, 56, 39, 63, 43, 56, 57, 8, 8, 8, 0]",
+            t2c={
+                '[1:3]':'#3174f0',
+                '[5:7]':'#e53125',
+                '[9:11]':'#fbb003',
+                '[13:15]': '#fbb003'
+            }
+        ).scale(.5)
+        self.play(manim.ReplacementTransform(quote, encoded_paragraph))
+        self.wait(7)
+        self.play(manim.FadeToColor(encoded_paragraph, color=manim.WHITE))
+        self.wait(3)
+        self.play(manim.FadeOut(encoded_paragraph))
+        self.wait(1)
+        self.next_section()
+        ###########################################
+        #                                         #
+        #       END SECTION ONE, ENCODING         #
+        #                                         #
+        ###########################################
+
+        ###########################################
+        #                                         #
+        #       BEGIN SECTION TWO, CONTEXT        #
+        #                                         #
+        ###########################################
+
+        # create a code class to imitate a document
+        # FADE IN: document
+        doc1_text = """Falstaff: My king, my Jove! I speak to thee, my heart!
+
+King Henry V: I know thee not, old man. Fall to thy prayers.
+How ill white hairs become a fool and jester!
+        """
+        doc1 = manim.Code(code=doc1_text, background="window", language="html", font_size=24)
+        self.play(manim.DrawBorderThenFill(doc1))
+        self.wait(3)
+        animations = [
+            manim.FadeToColor(doc1[2][2][40], color='#3174f0'),
+            manim.FadeToColor(doc1[2][2][41], color='#e53125'),
+            manim.FadeToColor(doc1[2][2][42:44], color='#fbb003')
+        ]
+        self.play(manim.AnimationGroup(*animations))
+        self.wait(5)
